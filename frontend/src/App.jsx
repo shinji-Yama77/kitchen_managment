853848d9dd725052a5f4d10a39bcf3bf2b6040e6
shinji-Yama77 <syamas@uw.edu>
2024-11-27 21:12:19 -0800

    home_page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from "react";
 import Home from './HomePage.jsx'
 import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar.jsx";
+import './index.css'
+
 
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
       <Navbar />
-    </>
-    
-    
-
+    </div>
+      
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,11 +4,14 @@ import styles from '../styles/Navbar.module.css';
 
 export default function Nav() {
     return (
-        <nav className={styles.container}>
-            <a>Current Inventory</a>
-            <a>Current Inventory</a>
-            <a>Current Inventory</a>
-            <a>Current Inventory</a>
+        <nav >
+            <ul className={styles.container}>
+                <li><a href='#'>Current Inventory</a></li>
+                <li><a href='#'>Your Recipes</a></li>
+                <li><a href='#'>Create a Recipe</a></li>
+                <li><a href='#'>Add a New Ingredient</a></li>
+            </ul>
         </nav>
     )
 }
+
